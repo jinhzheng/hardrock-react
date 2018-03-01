0b6f338387f3ba232256431440076be41aeea619
jinhuafromuhm <33620845+jinhuafromuhm@users.noreply.github.com>
2018-03-01 10:06:21 -1000

    .

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -49,7 +49,10 @@ class Bottom extends React.Component {
             </Grid.Column>
             <Grid.Column Container width={16}textAlign='center'>
               <Icon name="phone icon"/>
+              +1-808-955-7383
+              <Icon name='mail icon'/>
                 Email Us
+              <Icon name='home icon'/>
                 280 Beachwalk, Honolulu, Hawaii 96815
                 <Icon name="twitter icon"></Icon>
                 <Icon name="facebook f icon"></Icon>
